acd424d0e7b998ebc9bf6df4d999f1e8363a111d
Gilles Rousseaux <gilles.rousseaux@cubitec.be>
2023-12-14 14:54:34 +0000

    send email setup

--- pages/Home/jsobjects/send_emails/send_emails.js
+++ pages/Home/jsobjects/send_emails/send_emails.js
@@ -1,19 +1,28 @@
 export default {
-	userName : '',
 	userId : '',
+	userName : '',
 	userMail : '',
+	now: '',
 	
 	subject : '',
 	message : '',
 	
 	async sendEmailToUsers() {
+		const delay = 1000 * 60 * 60 * 24 * 5;
+    const now = new Date();
+		
 		for (const user of mail_users.data) {
+			this.userId = user._id;
 			this.userName = user.name;
 			this.userMail = user.email;
-			this.userId = user._id;
+			this.now = now;
 			
 			this.subject = 'GAP!';
 			this.message = user;
+			
+			const lastEmailSent = new Date(user.lastEmailSent);
+			if (now - lastEmailSent < delay) return;
+			update_mail_delay.run();
 			
 			try {
 				await send_email.run();
