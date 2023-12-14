1a8a0445b8fd84b68c30876f661803313c1dfd20
Gilles Rousseaux <gilles.rousseaux@cubitec.be>
2023-12-14 16:23:26 +0000

    automated emails

--- pages/Home/jsobjects/send_emails/send_emails.js
+++ pages/Home/jsobjects/send_emails/send_emails.js
@@ -1,35 +1,50 @@
 export default {
+	now: '',
 	userId : '',
 	userName : '',
 	userMail : '',
-	now: '',
-	
 	subject : '',
 	message : '',
 	
 	async sendEmailToUsers() {
-		const delay = 1000 * 60 * 60 * 24 * 5;
+    const delay = 1000 * 60 * 0 * 0 * 0; // 5 days in milliseconds // const delay = 1000 * 60 * 60 * 24 * 5; // 5 days in milliseconds
     const now = new Date();
-		
-		for (const user of mail_users.data) {
-			this.userId = user._id;
-			this.userName = user.name;
-			this.userMail = user.email;
-			this.now = now;
+
+    for (const user of mail_users.data) {
+        this.now = now.toISOString(); 
+        this.userId = user._id;
+        this.userName = user.name;
+        this.userMail = user.email;
+        this.subject = 'GAP TIME!';
 			
-			this.subject = 'GAP!';
-			this.message = user;
+				// mail message
+				const message = `
+hi, ${user.name}
+test bericht, hallo ik ben automated,
+zie je terug binnen 5 dagen.
+`;
 			
-			const lastEmailSent = new Date(user.lastEmailSent);
-			if (now - lastEmailSent < delay) return;
-			update_mail_delay.run();
+        this.message = message;
+
 			
-			try {
-				await send_email.run();
-				console.log(`Email sent to ${this.userName}`);
-			} catch (error) {
-				console.error(`Failed to send email to ${user.email}: ${error}`);
-			}
-				}
-    }
+				// rest of the code
+        const lastEmailSent = new Date(user.lastEmailSent);
+        const calc = now - lastEmailSent;
+
+        if (calc <= delay) {
+            console.log(`Skipping email to ${this.userName} - last email sent recently.`);
+            continue; // Skip sending an email if the last email was sent within the delay
+        }
+
+        // Update mail delay here
+        update_mail_delay.run(); // Ensure this operation completes if it's async
+
+        try {
+            await send_email.run();
+            console.log(`Email sent to ${this.userName}`);
+        } catch (error) {
+            console.error(`Failed to send email to ${user.email}: ${error}`);
+        }
+		}
+	}
 }
