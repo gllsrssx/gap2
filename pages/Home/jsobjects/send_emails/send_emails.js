6a52e86d11a0c781e008d7366bf91d573450797d
Gilles Rousseaux <gilles.rousseaux@cubitec.be>
2023-12-15 15:28:03 +0000

    fix mail spam

--- pages/Home/jsobjects/send_emails/send_emails.js
+++ pages/Home/jsobjects/send_emails/send_emails.js
@@ -15,35 +15,31 @@ export default {
         this.userId = user._id;
         this.userName = user.name;
         this.userMail = user.email;
-        this.subject = 'GAP TIME!';
-			
-				// mail message
-				const message = `
-hi, ${user.name}
-test bericht, hallo ik ben automated,
-zie je terug binnen 5 dagen.
-
-GAP @ ${appsmith.URL.fullPath}
-`;
-			
-        this.message = message;
-
+        this.subject = 'GAP TIME!';		
 			
 				// rest of the code
         const lastEmailSent = new Date(user.lastEmailSent);
         const calc = now - lastEmailSent;
 
         if (calc <= delay) {
-            // console.log(`Skipping email to ${this.userName} - last email sent recently.`);
+            console.log(`Skipping email to ${this.userName} - last email sent recently.`);
             continue; // Skip sending an email if the last email was sent within the delay
         }
 
+				// mail message
+				this.message = `
+				hi, ${user.name}
+				test bericht, hallo ik ben automated,
+				zie je terug binnen 5 dagen.
+
+				GAP @ ${appsmith.URL.fullPath}
+				`;
         // Update mail delay here
         update_mail_delay.run(); // Ensure this operation completes if it's async
 
         try {
             await send_email.run();
-            // console.log(`Email sent to ${this.userName}`);
+            console.log(`Email sent to ${this.userName}`);
         } catch (error) {
             console.error(`Failed to send email to ${user.email}: ${error}`);
         }
